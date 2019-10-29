0a6b58a3539b722f3cf92fb0785e2a4def4b590e
Nidzam81 <ijam1301@gmail.com>
2019-10-30 00:21:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ import plotly.graph_objs as go
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 app.config.suppress_callback_exceptions = True
 
 df=pd.read_csv(r'data_processed5.csv',
